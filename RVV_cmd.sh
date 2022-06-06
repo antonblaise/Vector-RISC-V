3aaaa2af170b7622bc3badf4199feeff1bf503e4
antonblaise <antoniusblaise@gmail.com>
2022-06-07 00:22:09 +0800

    Update RVV_cmd.sh

--- RVV_cmd.sh
+++ RVV_cmd.sh
@@ -2,6 +2,10 @@
 ### RISC-V Vector Extension - Compiler and Simulator commands ###
 #################################################################
 
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
 if [ -z ${RISCV+x} ]; then export RISCV=$HOME/local/riscv_v; fi
 
 rvv64-compile(){
@@ -23,4 +27,18 @@ rvv64-link(){
     $RISCV/gnu/bin/riscv64-unknown-elf-gcc $mainFile $riscvFileName -o $riscvFileName.o
     rm $riscvFileName
     printf ">> Output binary generated as ${riscvFileName}.o\n"
+}
+
+rvv64-help(){
+    printf """
+${YELLOW}                   RISC-V Vector Extension - Compiler and Simulator${NC}
+
+${WHITE}                                    Ha How Ung (2022)${NC}
+
+${WHITE}                                        Usage:${NC}
+
+rvv64-help\t\t\t\t\t\t- Show this help menu
+rvv64-compile\t<C/RISC-V script>\t\t\t- Compile the C/RISC-V script
+rvv64-run\t<RISC-V binary file>\t\t\t- Run simulation using the compiled RISC-V
+rvv64-link\t<main script> <RISC-V function script>\t- Compile with a main script that calls the function written in RISC-V"""
 }
